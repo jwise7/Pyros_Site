6cc84054b6f5e3e424ee6a654a5ff5b55e172339
jwise7 <jwise7@gmail.com>
2018-09-04 20:16:56 -0500

    data model second attempt

--- simple_tournament/models.py
+++ simple_tournament/models.py
@@ -6,13 +6,32 @@ class STournament(models.Model):
     tournament_name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-class Participant(models.Model):
+class STournament_Participant(models.Model):
+    tournament = models.ForeignKey('STournament')
     player_name = models.CharField(max_length=200)
 
-class STournamentTree(models.Model):
-    round = models.IntegerField(default=1)
-    order = models.IntegerField(default=0)
+class STournament_Match(models.Model):
+    match_num = models.IntegerField(default=1)
     tournament = models.ForeignKey('STournament')
-    participant1 = models.ForeignKey('Participant')
-    participant2 = models.ForeignKey('Participant')
-    winner = models.ForeignKey('Participant')
+    participant1 = models.ForeignKey('STournament_Participant')
+    participant2 = models.ForeignKey('STournament_Participant')
+    winner = models.ForeignKey('STournament_Participant')
+
+#NOTE: match_num combined with the total number of entrants should give all the information needed to create a single elimination bracket. Idea copied from: https://dba.stackexchange.com/questions/7887/best-way-to-design-tournament-database
+
+#EXAMPLE:
+#    A
+# match 1 -----+
+#    B         A
+#           match 5 -----+
+#    C         C         |
+# match 2 -----+         |
+#    D                   A
+#                     match 7
+#    E                   F
+# match 3 -----+         |
+#    F         F         |
+#           match 6 -----+
+#    G         G
+# match 4 -----+
+#    H
